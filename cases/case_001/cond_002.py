23c80daa1bd1fdda27f18c924442803c5314f5cf
sadredinmousavi <s.s.mousavi90@gmail.com>
2026-07-06 06:28:15 +0330

    update

--- cases/case_001/cond_002.py
+++ cases/case_001/cond_002.py
@@ -16,18 +16,17 @@ PARAMS = {
     #   (start_time, target_pos_1, target_pos_2)
     # Write the angle by hand in degrees and convert inline with np.deg2rad(...).
     "TARGET_SCHEDULE": [
-        # (0.0, *two_equilibrium_points([0.00, 0.00], np.deg2rad(80.0))),
-        (6.0, *two_equilibrium_points([0.00, 0.00], np.deg2rad(0.0))),
-        (12.0, *two_equilibrium_points([0.00, 0.00], np.deg2rad(45.0))),
-        (18.0, *two_equilibrium_points([0.00, 0.00], np.deg2rad(90.0))),
-        (24.0, *two_equilibrium_points([0.00, 0.00], np.deg2rad(135.0))),
-        (30.0, *two_equilibrium_points([0.00, 0.00], np.deg2rad(180.0))),
-        (36.0, *two_equilibrium_points([0.00, 0.00], np.deg2rad(225.0))),
-        (42.0, *two_equilibrium_points([0.00, 0.00], np.deg2rad(270.0))),
-        (48.0, *two_equilibrium_points([0.00, 0.00], np.deg2rad(315.0))),
-        (54.0, *two_equilibrium_points([0.00, 0.00], np.deg2rad(360.0))),
-        # (60.0, *two_equilibrium_points([0.001, 0.00], np.deg2rad(80.0))),
+        (6.0,  np.array([ 0.060,  0.000]), 1, np.deg2rad(0.0)),
+        (12.0, np.array([ 0.042,  0.042]), 1, np.deg2rad(45.0)),
+        (18.0, np.array([ 0.000,  0.060]), 1, np.deg2rad(90.0)),
+        (24.0, np.array([-0.042,  0.042]), 1, np.deg2rad(135.0)),
+        (30.0, np.array([-0.060,  0.000]), 1, np.deg2rad(180.0)),
+        (36.0, np.array([-0.042, -0.042]), 1, np.deg2rad(225.0)),
+        (42.0, np.array([ 0.000, -0.060]), 1, np.deg2rad(270.0)),
+        (48.0, np.array([ 0.042, -0.042]), 1, np.deg2rad(315.0)),
+        (54.0, np.array([ 0.060,  0.000]), 1, np.deg2rad(360.0)),
     ],
+
 
     # --- Initial Robot Positions ---
     "INITIAL_ROBOT_POSITIONS": np.array([
---
+++ cases/case_003/cond_004.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+PARAMS = {
+    "TARGET_SCHEDULE": [
+        (0.0,  np.array([ 0.00,  0.00]), 1, np.deg2rad(0.0)),
+        (6.0,  np.array([ 0.06,  0.00]), 1, np.deg2rad(0.0)),
+        (12.0, np.array([ 0.042,  0.042]), 1, np.deg2rad(45.0)),
+        (18.0, np.array([ 0.00,  0.06]), 1, np.deg2rad(90.0)),
+        (24.0, np.array([-0.042,  0.042]), 1, np.deg2rad(135.0)),
+        (30.0, np.array([-0.06,  0.00]), 1, np.deg2rad(180.0)),
+        (36.0, np.array([-0.042, -0.042]), 1, np.deg2rad(225.0)),
+        (42.0, np.array([ 0.00, -0.06]), 1, np.deg2rad(270.0)),
+        (48.0, np.array([ 0.042, -0.042]), 1, np.deg2rad(315.0)),
+        (54.0, np.array([ 0.06,  0.00]), 1, np.deg2rad(360.0)),
+        (60.0, np.array([ 0.00,  0.00]), 1, np.deg2rad(360.0)),
+    ],
+
+    "INITIAL_ROBOT_POSITIONS": np.array([
+        [-0.08, 0.04], [-0.05, 0.06], [-0.02, 0.08],
+        [0.02, 0.08], [0.05, 0.06], [0.08, 0.04],
+        [0.00, -0.09],
+    ]),
+}
